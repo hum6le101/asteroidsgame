d94fd71885604e3d671c5c956addf0a5bc72de56
hum6le101 <johanneshandrock@hotmail.com>
2025-05-07 20:47:08 +0200

    The first commit to the first game. This only starts up a black screen, that you can close

---
+++ main.py
@@ -0,0 +1,22 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+    number = 5
+    while number > 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
