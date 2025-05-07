d93b4922b1e1f668ef168ea60457eea3f02b2fb1
hum6le101 <johanneshandrock@hotmail.com>
2025-05-07 21:07:53 +0200

    Updating the Framerate

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def main():
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
     number = 5
+    clock = pygame.time.Clock()
+    dt = 0
     while number > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60)/1000)
 
 if __name__ == "__main__":
     main()
