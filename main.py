0921de19562b1a2d8136eb3d5e4c535a102c255e
hum6le101 <johanneshandrock@hotmail.com>
2025-05-07 22:04:15 +0200

    I created Groups, so I can more easily call additional objects with my methods.

--- main.py
+++ main.py
@@ -14,15 +14,19 @@ def main():
     number = 5
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while number > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
-        player.update(dt)
+        updatable.update(dt)
         player.move(dt)
-        player.draw(screen)
+        for dr in drawable:
+            dr.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = (clock.tick(60)/1000)
